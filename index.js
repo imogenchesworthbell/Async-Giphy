ad05f8991ba85d8593f7c28cda0efe41f7aaa1b9
eloise <eloisewilkinson@115.sub-97-13-190.myvzw.com>
2024-10-07 15:53:22 +0100

    complete upto extention

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
 require('dotenv').config();
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+// console.log(process.env.API_KEY)
+
+const apiKey = '36lW5ujA0bV2qymxm2xyaPDKsFsOtNYl';
+// const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}\&q=${encodeURIComponent(query)}\&limit=1\&offset=0\&rating=g\&lang=en`; 
+
+// console.log(apiKey)
+
+async function getImages(query){
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}\&q=${encodeURIComponent(query)}\&limit=1\&offset=0\&rating=g\&lang=en`;
+   // try {
+     //   const response = await fetch(endpoint)
+       // const data = await response.json()
+        //let url = data.data[0].url
+        //console.log(url)
+
+    // }catch(error){
+    //     console.error(error)
+   //  }
+
+   const response = await fetch(endpoint)
+   const data = await response.json()
+   let url = data.data[0].url 
+   console.log(url)
+
+    
+}
+getImages("dogs")
+
